8b61f1bc06ef3e13113e90f593a9a308dfe836f4
BulyginK <buligin.k@gmail.com>
2022-04-21 18:03:19 +0300

    начал выполнять задание 19 урока

--- src/modules/one.js
+++ src/modules/one.js
@@ -1,7 +1,20 @@
 'use strict';
 
 const one = () => {
-    console.log('one.js');
+    const timerHours = document.getElementById('timer-hours');
+    const timerMinutes = document.getElementById('timer-minutes');
+    const timerSeconds = document.getElementById('timer-seconds');
+    let deadline = '25 april 2022';
+    let dateStop = new Date(deadline).getTime();
+    let dateNow = new Date().getTime();
+    let timeRemaining = (dateStop - dateNow) / 1000;
+    let hours = Math.floor(timeRemaining / 60 / 60);
+    let minutes = Math.floor((timeRemaining / 60) % 60);
+    let seconds = Math.floor(timeRemaining % 60);
+
+    timerHours.textContent = hours;
+    timerMinutes.textContent = minutes;
+    timerSeconds.textContent = seconds;
 }
 
 export default one
