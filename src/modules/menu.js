fe79673ad5f30a492a474918dc2b263ca4a59e1a
BulyginK <bulygin.k@gmail.com>
2022-04-24 14:22:08 +0300

    правка показа #close

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -2,15 +2,16 @@ const menu = () => {
     const menuBtn = document.querySelector('.menu');
     const menu = document.querySelector('menu');
     const closeBtn = menu.querySelector('.close-btn');
-    const menuItems = menu.querySelectorAll('ul>li>a')
+    const menuItems = menu.querySelectorAll('ul>li>a');
      
-    const handleMenu = () => {
-        menu.classList.toggle('active-menu')
+    const handleMenu = (e) => {
+        if (e.target == closeBtn) {e.preventDefault()}
+        menu.classList.toggle('active-menu');
     }
 
     menuBtn.addEventListener('click', handleMenu);
     closeBtn.addEventListener('click', handleMenu);
-    menuItems.forEach(menuItems => menuItems.addEventListener('click', handleMenu))
+    menuItems.forEach(menuItems => menuItems.addEventListener('click', handleMenu));
 }
 
 export default menu
