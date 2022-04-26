9958968cd21b36b8d4f41cf33eff4ad16a88c14d
BulyginK <buligin.k@gmail.com>
2022-04-26 17:29:11 +0300

    выполнил 21 задание

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -1,10 +1,10 @@
 const calc = () => {
     const calcInput = document.querySelectorAll('#calc input');
-    const form = document.querySelectorAll('[name="user_form"]');
-    // const userName = document.querySelectorAll('[name="user_name"]');
-    // const formEmail = document.querySelectorAll('[name="user_email"]');
-    // const userPhone = document.querySelectorAll('[name="user_phone"]');
-    // const userMessage = document.querySelectorAll('[name="user_message"]');
+    const form = document.querySelectorAll('[name="user_form"]')
+    const userName = document.querySelectorAll('[name="user_name"]');
+    const formEmail = document.querySelectorAll('[name="user_email"]');
+    const userPhone = document.querySelectorAll('[name="user_phone"]');
+    const userMessage = document.querySelectorAll('[name="user_message"]');
 
     for (let i = 0; i < calcInput.length; i++) {
         calcInput[i].addEventListener('input', (e) => {
@@ -12,24 +12,37 @@ const calc = () => {
         });
     };
 
-    for (let i = 0; i < form.length; i++) {
-        form[i].addEventListener('submit', (e) => {
-            const userName = form[i].querySelector('.form-name');
-            const formEmail = form[i].querySelector('.form-email');
-            const userPhone = form[i].querySelector('.form-phone');
-            const userMessage = form[i].querySelector('.mess');
-            e.preventDefault()
-
-            if (!/^[А-Яа-я]+$/i.test(userName.value)) { alert('+') }
-            if (formEmail.value) { }
-            if (userPhone.value) { }
-            if (userMessage.value) { }
-        })
+    for (let i = 0; i < userName.length; i++) {
+        userName[i].addEventListener('input', (e) => {
+            if (!(/^[а-яА-Я\s-]+$/g.test(e.target.value))) {
+                e.target.value = e.target.value.replace(/.$/, "")
+            }
+        });
     };
 
+    for (let i = 0; i < formEmail.length; i++) {
+        formEmail[i].addEventListener('input', (e) => {
+            if (!(/^[-\w\d_\.\!~\*\'\@]+$/gi.test(e.target.value))) {
+                e.target.value = e.target.value.replace(/.$/, "")
+            }
+        });
+    };
 
+    for (let i = 0; i < userPhone.length; i++) {
+        userPhone[i].addEventListener('input', (e) => {
+            if (!(/^[\d)(-]+$/g.test(e.target.value))) {
+                e.target.value = e.target.value.replace(/.$/, "")
+            }
+        });
+    };
 
-
+    for (let i = 0; i < userMessage.length; i++) {
+        userMessage[i].addEventListener('input', (e) => {
+            if (!(/^[а-яА-Я\s-]+$/g.test(e.target.value))) {
+                e.target.value = e.target.value.replace(/.$/, "")
+            }
+        });
+    };
 }
 
 export default calc
