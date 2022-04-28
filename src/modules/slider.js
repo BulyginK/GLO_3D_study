ac66412533f684a3cc44ff973156bb200c4db69b
BulyginK <bulygin.k@gmail.com>
2022-04-28 13:59:50 +0300

    выполнил задание 23 урока

--- src/modules/slider.js
+++ src/modules/slider.js
@@ -1,16 +1,26 @@
 const slider = () => {
     const sliderBlock = document.querySelector('.portfolio-content');
     const sliders = document.querySelectorAll('.portfolio-item');
-
+    const dots = document.querySelectorAll('.dot');
     let currentSlide = 0;
 
+    const prevSlide = (elems, index, strClass) => {
+        elems[index].classList.remove(strClass);
+    }
+
+    const nextSlide = (elems, index, strClass) => {
+        elems[index].classList.add(strClass);
+    }
+
     const autoSlide = () => {
-        sliders[currentSlide].classList.remove('portfolio-item-active');
+        prevSlide(sliders, currentSlide, 'portfolio-item-active');
+        prevSlide(dots, currentSlide, 'dot-active');
         currentSlide++;
         if (currentSlide >= sliders.length) {
             currentSlide = 0;
         }
-        sliders[currentSlide].classList.add('portfolio-item-active');
+        nextSlide(sliders, currentSlide, 'portfolio-item-active');
+        nextSlide(dots, currentSlide, 'dot-active');
     };
 
     const startSlide = () => {
@@ -20,6 +30,40 @@ const slider = () => {
     const stopSlide = () => {
 
     };
+
+    sliderBlock.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        if (!e.target.matches('.dot, .portfolio-btn')) {
+            return
+        }
+
+        prevSlide(sliders, currentSlide, 'portfolio-item-active');
+        prevSlide(dots, currentSlide, 'dot-active');
+
+        if (e.target.matches('#arrow-right')) {
+            currentSlide++;
+        } else if (e.target.matches('#arrow-left')) {
+            currentSlide--;
+        } else if (e.target.classList.contains('dot')) {
+            dots.forEach((dot, index) => {
+                if (e.target === dot) {
+                    currentSlide = index;
+                }
+            })
+        };
+
+        if (currentSlide >= sliders.length) {
+            currentSlide = 0;
+        }
+
+        if (currentSlide < 0) {
+            currentSlide = sliders.length - 1;
+        }
+
+        nextSlide(sliders, currentSlide, 'portfolio-item-active');
+        nextSlide(dots, currentSlide, 'dot-active');
+    })
 
     startSlide()
 }
