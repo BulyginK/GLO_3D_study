1f577d3aa8bebc6b22121f9c95c87952d33ffca7
BulyginK <buligin.k@gmail.com>
2022-04-29 10:16:35 +0300

    выполнил задание 23 урока

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,22 +1,33 @@
 const menu = () => {
+    const body = document.querySelector('body');
     const menuBtn = document.querySelector('.menu');
     const menu = document.querySelector('menu');
     const closeBtn = menu.querySelector('.close-btn');
 
 
-    menuBtn.addEventListener('click', () => {
+    const classActive = () => {
         menu.classList.toggle('active-menu');
+    };
+
+    body.addEventListener('click', (e) => {
+        if (menu.classList.contains('active-menu') && e.target.closest('button')) {
+            e.preventDefault();
+            classActive();
+        };
+
+        if (menu.classList.contains('active-menu') && !e.target.closest('menu')) {
+            classActive();
+        };
+
+        if (menuBtn.contains(e.target) || e.target === closeBtn) {
+            e.preventDefault();
+            classActive();
+        };
+
+        if (e.target.matches('menu>ul>li>a')) {
+            classActive();
+        };
     });
-
-    menu.addEventListener('click', (e) => {
-        if (e.target === closeBtn) {
-            e.preventDefault()
-            menu.classList.toggle('active-menu');
-        } else if (e.target.matches('ul>li>a')) {
-            menu.classList.toggle('active-menu');
-        }
-    })
-
 }
 
 export default menu
