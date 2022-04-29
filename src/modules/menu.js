011fdcbff88aa58adaae7b7e6fca3b1be266eab0
BulyginK <bulygin.k@gmail.com>
2022-04-29 06:24:56 +0300

    поправил задание 22 урока

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,25 +1,64 @@
 const menu = () => {
+    const body = document.querySelector('body');
     const menuBtn = document.querySelector('.menu');
     const menu = document.querySelector('menu');
     const closeBtn = menu.querySelector('.close-btn');
+
 
     const classActive = () => {
         menu.classList.toggle('active-menu');
     };
 
-    menuBtn.addEventListener('click', (e) => {
-        e.preventDefault();
-        classActive();
-    });
+    body.addEventListener('click', (e) => {
+        if (menu.classList.contains('active-menu') && e.target.closest('button')) {
+            e.preventDefault();
+            classActive();
+        }
 
-    menu.addEventListener('click', (e) => {
+        if (menu.classList.contains('active-menu') && !e.target.closest('menu')) {
+            classActive();
+        }
+
+        if (menuBtn.contains(e.target)) {
+            e.preventDefault();
+            classActive()
+        };
+
         if (e.target === closeBtn) {
             e.preventDefault();
             classActive();
-        } else if (e.target.matches('ul>li>a')) {
+        };
+
+        if (e.target.matches('menu>ul>li>a')) {
             classActive();
-        }
+        };
     });
 }
 
 export default menu
+
+
+
+
+
+
+
+
+
+
+
+
+
+// menuBtn.addEventListener('click', (e) => {
+    //     e.preventDefault();
+    //     classActive();
+    // });
+
+    // menu.addEventListener('click', (e) => {
+    //     if (e.target === closeBtn) {
+    //         e.preventDefault();
+    //         classActive();
+    //     } else if (e.target.matches('ul>li>a')) {
+    //         classActive();
+    //     }
+    // });
