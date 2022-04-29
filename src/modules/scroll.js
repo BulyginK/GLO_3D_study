d9f70537fee8e1517e4733546420e698d150fdbe
BulyginK <buligin.k@gmail.com>
2022-04-29 13:58:44 +0300

    выполнил задание 23 урока

--- src/modules/scroll.js
+++ src/modules/scroll.js
@@ -1,12 +1,23 @@
 const scroll = () => {
-    const menu = document.querySelector('menu');
-    const itemMenu = menu.querySelectorAll('ul>li>a');
+    const itemMenu = document.querySelectorAll('menu>ul>li>a');
+    const scrollBtn = document.querySelector('main>a');
 
     itemMenu.forEach(item => {
         item.addEventListener('click', (e) => {
             e.preventDefault();
-            document.querySelector(item.hash).scrollIntoView(top);
-        })
+            document.querySelector(item.hash).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        });
+    });
+
+    scrollBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        document.querySelector(scrollBtn.hash).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
     });    
 }
 
